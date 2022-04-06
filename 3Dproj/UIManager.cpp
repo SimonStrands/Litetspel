752528aacb21ba5b48452cc09b584fa8af6207c2
Simon Strand <etta11@hotmail.se>
2022-04-06 16:10:47 +0200

    added UI Shaders

--- 3Dproj/UIManager.cpp
+++ 3Dproj/UIManager.cpp
@@ -22,3 +22,13 @@ void UIManager::createUISprite(std::string rmsprite, vec2 pos, vec2 size)
 {
 	elements.push_back(new UISprite(rm->getSprite(rmsprite, gfx), pos, size));
 }
+
+void UIManager::draw()
+{
+	for (int i = 0; i < elements.size(); i++) {
+		//elements[i].draw();
+	}
+	for (int i = 0; i < strings.size(); i++) {
+		strings[i].draw();
+	}
+}
--- 3Dproj/UIManager.h
+++ 3Dproj/UIManager.h
@@ -12,6 +12,7 @@ public:
 	virtual ~UIManager();
 	void createUIString(std::string str, vec2 pos, vec2 size);
 	void createUISprite(std::string rmsprite, vec2 pos, vec2 size);
+	void draw();
 private:
 	std::vector<UIElements*> elements;
 	std::vector<UIString> strings;
