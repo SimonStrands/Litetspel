cf91be01c54706204e65902fee847ea1bf75d2ad
Simon Strand <etta11@hotmail.se>
2022-04-12 14:11:43 +0200

    a

--- 3Dproj/GameObject.h
+++ 3Dproj/GameObject.h
@@ -3,7 +3,7 @@
 #include <string>
 #include "vertex.h"
 #include "object.h"
-
+#include <DirectXCollision.h>
 #include "Model.h"
 
 class GameObject : public object{
@@ -13,20 +13,34 @@ public:
 	
 	void draw(Graphics*& gfx, bool sm = false);
 
+	vec3 getlastPosition();
+
 	//update vertexConstBuffer on object
 	void Updateshaders(Graphics*& gfx, bool vertex = true);
 
 	//gives 2 points 0 = the lowest in x,y and z while 1 is the highest in x,y and zdimensions
 	void getBoundingBox(DirectX::XMVECTOR theReturn[]);
 
+	DirectX::BoundingBox getDirectXBoundingBoxFromModel();
+	DirectX::BoundingBox getDirectXBoundingBoxFromObject();
+	void setBoundingBox(DirectX::XMFLOAT3 pos, DirectX::XMFLOAT3 sizes);
+
 	//see if this object has beened drawned before(used for quad tree)
 	const bool isDrawed();
 
 	//clear that the object has not been drawned before
 	void clearDrawed();
+
+
+	//DEBUG
+	vec3 getWidthHeightDepth();
 protected:
 	ModelObj* model;
 private:
 	bool tess;
 	bool drawed;
+	DirectX::XMFLOAT3 BBpos;
+	DirectX::XMFLOAT3 BBsizes;
+	void setHeightWidthDepth();
+	vec3 WHD;
 };
